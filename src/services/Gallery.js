5ce48958e70297a84dbe501bd8179987c79f1a51
Subin Paul <spaul3@sapient.com>
2016-11-26 22:58:28 +0530

    removing https

--- src/services/Gallery.js
+++ src/services/Gallery.js
@@ -4,7 +4,7 @@ angular
 
 			var url = 'data/subinpaul.json';
 			if (location.href.indexOf("subinpaul.com") > -1 ) { // Firebase URL for live site
-				url = 'https://subinpaul-v3.firebaseio.com/portfolio.json';
+				url = '//subinpaul-v3.firebaseio.com/portfolio.json';
 		  }
 
 			getFromLocalStorage = function (subPath) {
