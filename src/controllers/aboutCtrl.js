a50794b918576fb0f248b19cb5e92e934a99d163
Subin Paul <spaul3@sapient.com>
2018-04-30 19:02:18 +0530

    removing v3 code

--- src/controllers/aboutCtrl.js
+++
@@ -1,6 +0,0 @@
-angular
-	.module ('app')
-	.controller ('aboutCtrl', ['$scope', function($scope) {
-			$scope.items = ['item1','item2','item3','item4' ];
-			$scope.title1 = "Boo!";
-	}]);
--- src/controllers/storiesCtrl.js
+++
@@ -1,6 +0,0 @@
-angular
-	.module ('app')
-	.controller ('storiesCtrl', ['$scope', function($scope) {
-			$scope.items = ['item1','item2','item3','item4' ];
-			$scope.title1 = "Boo!";
-	}]);
